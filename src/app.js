b80c896cd790a671b74f7913571a46f10d05ad48
Anul Mehta <anulmehta1848@gmail.com>
2025-02-07 23:34:06 +0530

    7-2-25

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-const express=require("express");
-const app=express();
- 
-app.get("/user",(req,res)=>{
-    res.send({"fisrtname":"Anul","Lastname":"Mehta"})
-})
-app.post("/user",(req,res)=>{
-    res.send("Data Successfully saved to the Database")
-})
-app.use("/",(req,res)=>{
-    res.send("Hello Hello Hello!!!")
-})
+const express = require("express");
+const app = express();
 
-app.listen(7777,()=>{
-    console.log("server is running on port 7777");
-    
-})
+app.get(
+  "/user",
+  (req, res,next) => {
+    console.log("Handling the route user!!"); 
+    next()
+    res.send("Respone"); 
+  },
+  (req, res) => {
+    console.log("Handling the route user 2!!");
+    res.send("Respone 2");
+  }
+);
+
+app.listen(7777, () => {
+  console.log("server is running on port 7777");
+});
